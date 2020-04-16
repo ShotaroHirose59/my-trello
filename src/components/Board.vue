a1638afac46152e5faf5db373117df2f6a55f9aa
Shotaro Hirose <shotaro199959@icloud.com>
2020-04-16 23:59:29 +0900

    ListAdd, Listコンポーネントを作成

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,11 +5,30 @@
     </header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <list v-for="(item, index) in lists"
+            :key="item.id"
+            :title="item.title"
+            :listIndex="index"
+      />
+      <list-add/>
     </main>
   </div>
 </template>
 
 <script>
+import List from './List'
+import ListAdd from './ListAdd'
+import { mapState } from 'vuex'
+
 export default {
+  components: {
+    ListAdd,
+    List,
+  },
+  computed: {
+    ...mapState([
+      'lists'
+    ]),
+  },
 }
 </script>
